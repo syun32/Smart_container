f1f50415e77196c464b00b7e8d34c9c2a235745a
chocoo98 <chocoo98@naver.com>
2019-12-29 01:14:36 +0900

    Create temp.php
    show temperature

---
+++ Webpage/temp.php
@@ -0,0 +1,65 @@
+<?php
+	include ('dbcon.php');
+	include ('check.php');
+	include ('head.php');
+
+
+
+	$stmt = $con->prepare('SELECT update_time, temp, cool FROM containers WHERE ID=:con_num');
+	$stmt -> bindParam(":con_num",$_SESSION['con_num']);
+	$stmt -> execute();
+
+	$count = $stmt->rowCount();
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+	$i=0;
+	while($row = $stmt->fetch()){
+		$con_time[$i]=$row['update_time'];
+		$con_temp[$i]=$row['temp'];
+		$con_cool[$i]=$row['cool'];
+		$i++;
+	}
+?>
+<!DOCTYPE html>
+<html lang="ko">
+    <head> 
+        <meta http-equiv="Content-Type"  content="text/html; charset=UTF-8" /> 
+        <title>container information</title>
+        <style>
+        body{
+		font-family: Consolas, monospace;
+		font-family: 12px;
+	}
+	table{
+		width: 50%;
+	}
+	th, td{
+		padding: 10px;
+		border-bottom: 1px solid #dadada;
+	}
+        </style>
+    </head>
+    <body>
+
+	<div align="center">
+
+        <h3>CONTAINER ID : <?php echo $_SESSION['con_num'];?></h3>
+        <table>
+	<thead>
+		<tr bgcolor=#ffffbb>
+		<th>TIME</th>
+		<th>TEMPERATURE</th>
+		<th>COOLER</th>
+		</tr>
+	<thead>
+	<tbody>
+<?php
+	for($i=0;$i<$count;$i++){
+		echo "<tr><td>". $con_time[$i]."</td><td>".$con_temp[$i]."</td><td>".$con_cool[$i]."</td></tr>";
+	}
+?>
+
+	</tbody>
+        </table>
+    </body>
+</html>
